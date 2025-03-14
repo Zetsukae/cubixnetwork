7ca04525669ca798e04293639b07b51ee147fe1d
Addddwisk <alexis.dangelo@edu-vd.ch>
2025-03-14 12:31:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function copyIP() {
-    const ip = "178.194.224.249"; // ip
+    const ip = "178.194.224.249"; // Remplace par ton IP
     navigator.clipboard.writeText(ip).then(() => {
         document.getElementById("copied").classList.remove("hidden");
         setTimeout(() => {
             document.getElementById("copied").classList.add("hidden");
         }, 2000);
     });
+}
+
+// Changement d'onglet
+function showTab(tabName) {
+    document.querySelectorAll('.tab').forEach(tab => tab.classList.add('hidden'));
+    document.getElementById(tabName).classList.remove('hidden');
 }
